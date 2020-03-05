66c447b7cdba2613e101997459b860690efc62ab
Lilitdav <djoumbe.alisadoni.data@gmail.com>
2020-03-05 21:12:37 +0100

    Open classroom trainnig

---
+++ Exo_Open_revision.py
@@ -0,0 +1,18 @@
+#Exercices OpenCLassRoom révision
+
+rappeur = "Post Malone"
+print(rappeur)
+
+year = 1998
+print(year)
+
+print(9999 > 3333)
+
+list_rappeur = ["Xxxtentation", "Lil Pump", "Kanye West" , "Booba" , "Kaaris"]
+print (list_rappeur[2])
+
+Characters_univers = ("Kefla", "Goku", "Naruto" , "Ultra Instinct")
+print (Characters_univers[3])
+
+dictionnaire = {"Naruto":"Kyubi" , "Boruto":"Tensegan" }
+print(dictionnaire["Naruto"])
